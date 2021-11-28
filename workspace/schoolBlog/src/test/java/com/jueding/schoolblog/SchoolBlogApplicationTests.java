79c617a85734d50ddcc6cab323e26792514ab513
SuZui-cn <59428115+SuZui-cn@users.noreply.github.com>
2021-11-29 01:02:50 +0800

    Changes to be committed:

--- workspace/schoolBlog/src/test/java/com/jueding/schoolblog/SchoolBlogApplicationTests.java
+++ workspace/schoolBlog/src/test/java/com/jueding/schoolblog/SchoolBlogApplicationTests.java
@@ -1,13 +1,19 @@
 package com.jueding.schoolblog;
 
+import com.jueding.schoolblog.mapper.UserDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SchoolBlogApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
     void contextLoads() {
+        System.out.println(userDao.showAll());;
     }
 
 }
